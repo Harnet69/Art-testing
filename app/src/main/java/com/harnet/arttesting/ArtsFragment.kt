d3ae9035f3f79f0761bf7ae16c35f6c3bddbf51a
Harnet69 <harnet69@gmail.com>
2021-02-28 18:40:25 +0100

    Added arts fragment

---
+++ app/src/main/java/com/harnet/arttesting/ArtsFragment.kt
@@ -0,0 +1,31 @@
+package com.harnet.arttesting
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+class ArtsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ArtsFragment()
+    }
+
+    private lateinit var viewModel: ArtsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.arts_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ArtsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/harnet/arttesting/ArtsViewModel.kt
@@ -0,0 +1,7 @@
+package com.harnet.arttesting
+
+import androidx.lifecycle.ViewModel
+
+class ArtsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
