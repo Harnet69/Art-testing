301b69945ccb0a3b21a3338ba4a669a779ffdf4c
Harnet69 <harnet69@gmail.com>
2021-03-02 17:54:39 +0100

    Implemented FragmentBindingProvider.kt

--- app/src/main/java/com/harnet/arttesting/view/ArtAddingFragment.kt
+++ app/src/main/java/com/harnet/arttesting/view/ArtAddingFragment.kt
@@ -2,17 +2,40 @@ package com.harnet.arttesting.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.harnet.arttesting.R
+import com.harnet.arttesting.databinding.ArtAddingFragmentBinding
 import com.harnet.arttesting.viewModel.ArtAddingViewModel
 
 class ArtAddingFragment : Fragment(R.layout.art_adding_fragment) {
     private lateinit var viewModel: ArtAddingViewModel
+    private var viewBinding: ArtAddingFragmentBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(ArtAddingViewModel::class.java)
+        viewBinding = ArtAddingFragmentBinding.bind(view)
+
+        viewBinding.let {
+            if (it != null) {
+                goToArtSearch(it.artImageArtAdding)
+            }
+        }
+    }
+
+    private fun goToArtSearch(img: ImageView) {
+        img.setOnClickListener {
+            Navigation.findNavController(img)
+                .navigate(ArtAddingFragmentDirections.actionArtAddingFragmentToSearchFragment())
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewBinding = null
     }
 
 }
--- app/src/main/java/com/harnet/arttesting/view/SearchFragment.kt
+++ app/src/main/java/com/harnet/arttesting/view/SearchFragment.kt
@@ -4,23 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.harnet.arttesting.R
 import com.harnet.arttesting.databinding.SearchFragmentBinding
+import com.harnet.arttesting.util.FragmentBindingProvider
 import com.harnet.arttesting.viewModel.SearchViewModel
 
 class SearchFragment : Fragment() {
     private lateinit var viewModel: SearchViewModel
-    private lateinit var dataBinding: SearchFragmentBinding
+    private val binding: SearchFragmentBinding by FragmentBindingProvider(R.layout.search_fragment)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        dataBinding = DataBindingUtil.inflate(inflater, R.layout.search_fragment, container, false)
-        return dataBinding.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
