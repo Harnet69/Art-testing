9f88ded68e0798e7d046bdc71cf1d310b983a0cd
Harnet69 <harnet69@gmail.com>
2021-03-04 14:06:58 +0100

    Created Repository

--- app/src/main/java/com/harnet/arttesting/repository/ArtRepository.kt
+++ app/src/main/java/com/harnet/arttesting/repository/ArtRepository.kt
@@ -1,4 +1,41 @@
 package com.harnet.arttesting.repository
 
-class ArtRepository {
+import androidx.lifecycle.LiveData
+import com.harnet.arttesting.model.ImageResponse
+import com.harnet.arttesting.retrofit.RetrofitAPI
+import com.harnet.arttesting.room.Art
+import com.harnet.arttesting.room.ArtDao
+import com.harnet.arttesting.util.Resource
+import javax.inject.Inject
+
+class ArtRepository @Inject constructor(
+    private val artDao: ArtDao,
+    private val retrofitAPI: RetrofitAPI
+) : ArtRepositoryInterface {
+    override suspend fun insertArt(art: Art) {
+        artDao.insertArt(art)
+    }
+
+    override suspend fun deleteArt(art: Art) {
+        artDao.deleteArt(art)
+    }
+
+    override fun getAllArts(): LiveData<List<Art>> {
+        return artDao.getAllArts()
+    }
+
+    override suspend fun searchImg(imageString: String): Resource<ImageResponse> {
+        return try {
+            val response = retrofitAPI.searchImg(imageString)
+            if(response.isSuccessful){
+                response.body().let {data ->
+                    return@let Resource.success(data)
+                } ?: Resource.error("Error", null)
+            }else{
+                Resource.error("Error", null)
+            }
+        } catch (e : Exception){
+            Resource.error("No data", null)
+        }
+    }
 }
--- app/src/main/java/com/harnet/arttesting/repository/ArtRepositoryInterface.kt
+++ app/src/main/java/com/harnet/arttesting/repository/ArtRepositoryInterface.kt
@@ -5,6 +5,7 @@ import com.harnet.arttesting.model.ImageResponse
 import com.harnet.arttesting.room.Art
 import com.harnet.arttesting.util.Resource
 
+// created by the testing reason. Allows to get a fake ArtRepository
 interface ArtRepositoryInterface {
     suspend fun insertArt(art: Art)
 
@@ -12,5 +13,6 @@ interface ArtRepositoryInterface {
 
     fun getAllArts(): LiveData<List<Art>>
 
+    // assign success, error or loading state to ImageResponse
     suspend fun searchImg(imageString: String): Resource<ImageResponse>
 }
