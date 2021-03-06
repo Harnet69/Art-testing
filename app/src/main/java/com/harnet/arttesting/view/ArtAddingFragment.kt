0be19232487e507c828752ca08c9625dc5ad8fef
Harnet69 <harnet69@gmail.com>
2021-03-06 09:46:36 +0100

    Implemented adding art fragment

--- app/src/main/java/com/harnet/arttesting/view/ArtAddingFragment.kt
+++ app/src/main/java/com/harnet/arttesting/view/ArtAddingFragment.kt
@@ -3,12 +3,17 @@ package com.harnet.arttesting.view
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.RequestManager
+import com.google.android.material.button.MaterialButton
 import com.harnet.arttesting.R
 import com.harnet.arttesting.databinding.ArtAddingFragmentBinding
+import com.harnet.arttesting.util.Status
 import com.harnet.arttesting.viewModel.ArtAddingViewModel
 import javax.inject.Inject
 
@@ -25,15 +30,65 @@ class ArtAddingFragment @Inject constructor(
 
         viewBinding.let {
             if (it != null) {
-                goToArtSearch(it.artImageArtAdding)
+                goToArtSearchListener(it.artImageArtAdding)
             }
+        }
+
+        //handling onBackPress dispatcher
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
+        // add the art to a database listener
+        viewBinding?.artAddBtnArtAdding?.let { addArtListener(it) }
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.selectedImgUrl.observe(viewLifecycleOwner, { imgUrl ->
+            viewBinding?.let {
+                glide.load(imgUrl).into(it.artImageArtAdding)
+            }
+        })
+
+        // handling the state of an art loading
+        viewModel.mInsertArtMsg.observe(viewLifecycleOwner, { state ->
+            when (state.status) {
+                Status.SUCCESS -> {
+                    Toast.makeText(requireContext(), "Successfully added", Toast.LENGTH_SHORT).show()
+                    //TODO can be a problem
+                    findNavController().popBackStack()
+                    //reset status of loading to neutral
+                    viewModel.resetInsertArtMessage()
+                }
+                Status.LOADING -> {
+                    Toast.makeText(requireContext(), "Loading...", Toast.LENGTH_SHORT).show()
+                }
+                Status.ERROR -> {
+                    Toast.makeText(requireContext(), state.message ?: "Unexpected load error", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+    }
+
+    private fun goToArtSearchListener(imageView: ImageView) {
+        imageView.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(ArtAddingFragmentDirections.actionArtAddingFragmentToSearchFragment())
         }
     }
 
-    private fun goToArtSearch(img: ImageView) {
-        img.setOnClickListener {
-            Navigation.findNavController(img)
-                .navigate(ArtAddingFragmentDirections.actionArtAddingFragmentToSearchFragment())
+    private fun addArtListener(addBtn: MaterialButton) {
+        addBtn.setOnClickListener {
+            viewBinding?.let {
+                viewModel.validateUserInput(
+                    it.artNameArtAdding.text.toString(),
+                    it.artAuthorArtAdding.text.toString(),
+                    it.artYearArtAdding.text.toString()
+                )
+            }
         }
     }
 
--- app/src/main/java/com/harnet/arttesting/viewModel/ArtAddingViewModel.kt
+++ app/src/main/java/com/harnet/arttesting/viewModel/ArtAddingViewModel.kt
@@ -11,16 +11,16 @@ import kotlinx.coroutines.launch
 
 class ArtAddingViewModel @ViewModelInject constructor(private val repository: ArtRepositoryInterface) : ViewModel() {
     //keeping the state of image loading?
-    var insertArtMsg = MutableLiveData<Resource<Art>>()
+    var mInsertArtMsg = MutableLiveData<Resource<Art>>()
     val selectedImgUrl = MutableLiveData<String>()
 
-    fun insertImageToDb(art: Art) = viewModelScope.launch {
+    private fun insertImageToDb(art: Art) = viewModelScope.launch {
         repository.insertArt(art)
     }
 
     // when the art was successfully added to a database
     fun resetInsertArtMessage(){
-        insertArtMsg = MutableLiveData<Resource<Art>>()
+        mInsertArtMsg = MutableLiveData<Resource<Art>>()
     }
 
     // when a user clicks on an image
@@ -35,19 +35,19 @@ class ArtAddingViewModel @ViewModelInject constructor(private val repository: Ar
 
     fun validateUserInput(artName: String, artAuthor: String, artYear: String){
         if(artName.isEmpty() || artAuthor.isEmpty() || artYear.isEmpty()){
-            insertArtMsg.postValue(Resource.error("Field can't be empty", null))
+            mInsertArtMsg.postValue(Resource.error("Field can't be empty", null))
             return
         }
         val yearInt = try {
             artYear.toInt()
         }catch (e: Exception){
-            insertArtMsg.postValue(Resource.error("Year should be a number", null))
+            mInsertArtMsg.postValue(Resource.error("Year should be a number", null))
             return
         }
         //if selectedImgUrl isn't valid add empty string
         val art = Art(artName, artAuthor, selectedImgUrl.value ?: "", yearInt)
         insertImageToDb(art)
         setSelectedImg("")
-        insertArtMsg.postValue(Resource.success(art))
+        mInsertArtMsg.postValue(Resource.success(art))
     }
 }
