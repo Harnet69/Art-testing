f694d43425dd5825df3a48ba3bfa51d409468427
Harnet69 <harnet69@gmail.com>
2021-03-07 09:50:37 +0100

    ViewModel testing was implemented

--- app/src/test/java/com/harnet/arttesting/viewModel/ArtAddingViewModelTest.kt
+++ app/src/test/java/com/harnet/arttesting/viewModel/ArtAddingViewModelTest.kt
@@ -1,8 +1,10 @@
 package com.harnet.arttesting.viewModel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
 import com.harnet.arttesting.MainCoroutineRule
 import com.harnet.arttesting.repository.FakeArtRepository
+import com.harnet.arttesting.util.Status
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Rule
@@ -30,17 +32,35 @@ class ArtAddingViewModelTest {
         viewModel.validateUserInput("", "Da Vinci", "1500")
         //this value is LiveData which should be converted to a regular data by Google class method
         val value = viewModel.mInsertArtMsg.getOrAwaitValueTest()
-        println("Testing" + value.toString())
+        assertThat(value.status).isEqualTo(Status.ERROR)
     }
 
     @Test
     fun `insert art without art author returns error`(){
         viewModel.validateUserInput("Mona Lisa", "", "1500")
+        val value = viewModel.mInsertArtMsg.getOrAwaitValueTest()
+        assertThat(value.status).isEqualTo(Status.ERROR)
     }
 
     @Test
     fun `insert art without year returns error`(){
         viewModel.validateUserInput("Mona Lisa", "Da Vinci", "")
+        val value = viewModel.mInsertArtMsg.getOrAwaitValueTest()
+        assertThat(value.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert art with literal year returns error`(){
+        viewModel.validateUserInput("Mona Lisa", "Da Vinci", "XXYY")
+        val value = viewModel.mInsertArtMsg.getOrAwaitValueTest()
+        assertThat(value.message).isEqualTo("Year should be a number")
+    }
+
+    @Test
+    fun `set image URL set SelectedImgUrl`(){
+        viewModel.setSelectedImg("http://www.someimage.png")
+        val value = viewModel.selectedImgUrl.getOrAwaitValueTest()
+        assertThat(value).isEqualTo("http://www.someimage.png")
     }
 
 }
